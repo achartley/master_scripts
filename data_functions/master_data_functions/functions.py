7f5d9854510f102a1708c0c0560428051ae14225
Geir Tore Ulvik <ulvik@luring.uio.no>
2019-08-10 16:51:06 +0200

    import_data function working.

--- data_functions/master_data_functions/functions.py
+++ data_functions/master_data_functions/functions.py
@@ -1,6 +1,70 @@
+import os
 import numpy as np
 
 
+def import_data(folder='sample'):
+    """ Imports scintillator data as numpy arrays.
+    Used together with analysis repository which has a strict folder
+    structure.
+
+    param folder:   Which data to load. Defaults to 'sample'.
+        'real'      ->  real data from scintillator experiment
+        'sample'    ->  sample data (simulated)
+        'simulated' ->  simulated data from scintillator
+                    
+
+    returns:    dictionary of data where each filenames are keys and each
+                key,value pair contains dictionary of the data in the file,
+                separated into 'energies', 'positions', 'images', 'labels'.
+    """
+
+    # Verify input and set DATA_PATH
+    if folder == 'real':
+        DATA_PATH = '../../data/real/'
+    elif folder == 'sample':
+        DATA_PATH = '../../data/sample/'
+    elif folder == 'simulated':
+        DATA_PATH = '../../data/simulated/'
+    else:
+        print("Invalid data folder specified.") 
+        print("Must be 'real', 'sample', or 'simulated'")
+        exit(1)
+
+    # Get individual file paths and filenames from data folder
+    # Load each datafile into a dictionary, using the filenames as keys
+    # Each key then contains a dict with keys 'energies', 'positions'
+    # 'images', and 'labels'
+    data = {}
+
+    data_dir = os.scandir(DATA_PATH)
+    for FILE_PATH in data_dir:
+        filename = os.path.splitext(FILE_PATH.name)[0] # Don't need file extension
+
+        # Skip this file if it's the README file
+        if "README" in filename:
+            continue
+
+        # Load data
+        full_data = np.loadtxt(FILE_PATH.path)
+        separated_data = {}
+        
+        # Currently uncertain if real data has same form as simulated.
+        # If so, the if-test here can be discarded, and functions for
+        # simulated data can be renamed to something more generic.
+
+        if folder in ['simulated', 'sample']:
+            images, energies, positions = separate_simulated_data(full_data)
+            labels = label_simulated_data(full_data)
+
+            separated_data["images"] = images
+            separated_data["energies"] = energies
+            separated_data["positions"] = positions
+            separated_data["labels"] = labels
+
+        data[filename] = separated_data
+
+    return data
+    
 def separate_simulated_data(data):    
     """Takes an imported dataset and separates it into images, energies 
     and positions. Could potientially be expanded to return e.g a Pandas
@@ -32,7 +96,7 @@ def separate_simulated_data(data):
     
     return [images, energies, positions]
 
-def label_simulated_data(energies):
+def label_simulated_data(data):
     """Given arrays of energies, produces a set of labels for the dataset 
     for use in classification with
     0 -> single event
@@ -40,24 +104,16 @@ def label_simulated_data(energies):
     The labels are determined from the energy values, as we know that if 
     there is no second particle then Energy2 = 0."""
     
-    n_samples = energies.shape[0]
-    #n_classes = energies.shape[1]
+    # Extract energies as array with columns [Energy1, Energy2]
+    n_samples = data.shape[0]
+    n_pixels = 256 # The images are a 16x16 grid, flattened
+    energy1 = data[:, n_pixels].reshape(n_samples, 1) # reshape for stacking
+    energy2 = data[:, n_pixels+3].reshape(n_samples, 1) # reshape for stacking
+    energies = np.hstack((energy1, energy2))
 
-    #labels = np.zeros((n_samples))
-    # returns an n_samples x n_classes array where if the condition is true the value is 1
-    # and if the condition is false the value is 0, corresponding to
-    # double and single events, respectively.
-    # np.where(condition, return if true, return if false)
     labels = np.where(energies[:,1] != 0, 1, 0)
-    #labels[:,1] = np.where(energies[:,1] != 0, 1, 0)
             
     return labels
 
-def import_simulated_data(path_to_data):
-    """ Import simulated scintillator data
-
-    param path_to_data: if provided, import all datafiles present in folder.
-    """
-def import_simulated_sample():
-    """ Import small sample of simulated data containing several
-    """
+if __name__ == "__main__":
+   print(import_data())
