e9a090a5db18fa2bd55370aaa6dbe2a8ba262b97
Geir Tore Ulvik <ulvik@luring.uio.no>
2019-08-10 13:21:12 +0200

    Setup of functions using develop mode now works.

---
+++ data_functions/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='master_data_functions',
+    version='0.1.0',
+    author='Geir Tore Ulvik',
+    author_email='g.t.ulvik@fys.uio.no',
+    description='Scripts and functions for scintillator data import and preparation',
+    install_requires=[
+        "Numpy >= 1.17",
+    ],
+)
