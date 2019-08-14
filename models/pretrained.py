5ec658d00654da53a404cc8a00c9e670f4c71255
Geir Ulvik <ulvik@usit.uio.no>
2019-08-14 13:02:43 +0200

    Added function for setting up a vgg_model for scintillator data

---
+++ models/pretrained.py
@@ -0,0 +1,29 @@
+import numpy as np
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+from keras.engine.input_layer import Input
+
+model = VGG16(weights='imagenet', include_top=False, classes=2)
+
+def vgg_model(input_dim=(16, 16, 3), output_depth=0):
+    """ Setup an instance of the VGG16 model with new input dimensions
+        and a custom depth at which to get the output.
+
+        param input_dim:    dimensions of input, (16,16,3) is default for scintillator
+        param output_depth: At which layer to get the output from. 0 corresponds
+                            to the full model up to, but not including the
+                            dense layers. ("include_top=False")
+    """
+    input_layer = Input(shape=input_dim)
+    vgg = VGG16(include_top=False, input_tensor=input_layer, weights='imagenet')
+
+    # Set output_layer to full model, or to the desired depth if provided.
+    if output_depth > 0:
+        output_layer = Flatten()(vgg.layers[-1].output)
+    else:
+        output_layer = Flatten()(vgg.layers[output_depth].output)
+
+    # Keras infers the tensors between input and output layers.
+    model = Model(inputs=input_layer, outputs=output_layer) 
+
+    return model
