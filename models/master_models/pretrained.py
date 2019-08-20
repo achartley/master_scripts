18de89ccf93907f13c9d561f428b53721c25e583
Geir Ulvik <ulvik@usit.uio.no>
2019-08-20 15:21:16 +0200

    Pretrained vgg-model can now be loaded with desired depth:

--- models/master_models/pretrained.py
+++ models/master_models/pretrained.py
@@ -2,10 +2,10 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras import Input
-from tensorflow.keras.models import Model
+from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.layers import Dense, Flatten
 
-def vgg_model(input_dim=(16, 16, 3), output_depth=0):
+def vgg_model(input_dim=(16, 16, 3), output_depth=9):
     """ Setup an instance of the VGG16 model with new input dimensions
         and a custom depth at which to get the output. The goal of this
         model is to extract features from the new input using VGG's existing
@@ -17,16 +17,19 @@ def vgg_model(input_dim=(16, 16, 3), output_depth=0):
                             to the full model up to, but not including the
                             dense layers. ("include_top=False")
     """
+    # Create new input layer
     input_layer = Input(shape=input_dim)
+
+    # Load vgg16
     vgg = VGG16(include_top=False, weights='imagenet')
 
-    # Set output_layer to full model, or to the desired depth if provided.
-    if output_depth > 0:
-        output_layer = Flatten()(vgg.layers[output_depth].output)
-    else:
-        output_layer = Flatten()(vgg.layers[6].output)
+    # Add input layer and desired amount of vgg16 layers to new model
+    model = Sequential()
+    model.add(input_layer)
+    for i in range(1, output_depth):
+        model.add(vgg.layers[i])
 
-    # Keras infers the tensors between input and output layers.
-    model = Model(inputs=input_layer, outputs=output_layer) 
+    # Flatten layer to prep for inputting to dense
+    model.add(Flatten())
 
     return model
