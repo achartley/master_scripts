2df2877908c64c243c4e46258c535680c9e655e8
Geir Ulvik <ulvik@usit.uio.no>
2019-10-02 19:27:47 +0200

    Added labollita model to classification models.

---
+++ models/master_models/classification.py
@@ -0,0 +1,36 @@
+import numpy as np
+from importlib import import_module
+import tensorflow as tf
+import tensorflow.keras.applications as tfapps
+from tensorflow.keras import Input
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
+
+def labollita_model(compiled=True):
+    """ Setup an instance of the model made by Harrison Labollita in a previous
+        experiment with scintillator data.
+
+        :param compiled:    bool, if true compiled with same settings as in the
+                            project. If false, compilation must be done outside
+                            this function.
+
+        :return:    Instance of the Sequential model.
+    """
+
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3,3), activation = 'relu', input_shape= (16,16,1)))
+    model.add(Conv2D(64, (3,3), activation = 'relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.25))
+    model.add(Dense(128, activation = 'relu'))
+    model.add(Dropout(0.5))
+    model.add(Flatten())
+    model.add(Dense(2, activation = 'softmax'))
+    if compiled:
+        model.compile(
+                loss = 'categorical_crossentropy',
+                optimizer = 'adadelta',
+                metrics = ['accuracy']
+                )
+
+    return model
