e2fc9372418658ed9fc7a5a17273c845faa5be40
Geir Ulvik <ulvik@usit.uio.no>
2019-11-05 17:29:43 +0100

    Adding prediction model.

---
+++ models/master_models/prediction.py
@@ -0,0 +1,34 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.layers import Dense, Flatten, Dropout, Activation, Conv2D, MaxPooling2D
+
+def position_model(input_shape=(16,16,1)):
+    """ Set up a sequential model for prediction of positions.
+    """
+    model = Sequential()
+
+    # Add layers
+    model.add(Conv2D(32, (3, 3), padding='same',
+                     input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    #model.add(MaxPooling2D(pool_size=(2, 2)))
+    #model.add(Dropout(0.25))
+
+    #model.add(Conv2D(64, (3, 3), padding='same'))
+    #model.add(Activation('relu'))
+    #model.add(Conv2D(64, (3, 3)))
+    #model.add(Activation('relu'))
+    #model.add(MaxPooling2D(pool_size=(2, 2)))
+    #model.add(Dropout(0.25))
+    
+    model.add(Flatten())
+    #model.add(Dense(512))
+    #model.add(Activation('relu'))
+    #model.add(Dropout(0.5))
+    model.add(Dense(4))
+    model.add(Activation('linear'))
+
+    return model
