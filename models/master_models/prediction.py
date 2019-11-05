8f8a634e97fa2142bb49b07cff5eec58064eaa9f
Geir Ulvik <ulvik@usit.uio.no>
2019-11-05 20:26:49 +0100

    Added dense prediction model

--- models/master_models/prediction.py
+++ models/master_models/prediction.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.layers import Dense, Flatten, Dropout, Activation, Conv2D, MaxPooling2D
 
-def position_model(input_shape=(16,16,1)):
+def position_cnn(input_shape=(16,16,1)):
     """ Set up a sequential model for prediction of positions.
     """
     model = Sequential()
@@ -25,6 +25,26 @@ def position_model(input_shape=(16,16,1)):
     model.add(Dropout(0.25))
     
     model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(4))
+    model.add(Activation('linear'))
+
+    return model
+
+def position_dense(input_shape=(256, 1)):
+    """ Set up a sequential model for prediction of positions
+    using a fully-connected network.
+    """
+    model = Sequential()
+
+    # Add layers
+    model.add(Dense(512), input_shape=input_shape)
+    model.add(Activation('relu'))
+    model.add(Dense(512), input_shape=input_shape)
+    model.add(Activation('relu'))
+    model.add(Dropout(0.25))
     model.add(Dense(512))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
