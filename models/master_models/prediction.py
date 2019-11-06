398070249600e96770927661eb3fe4890845a346
Geir Ulvik <ulvik@usit.uio.no>
2019-11-06 12:25:19 +0100

    Added a version of the project model to prediction

--- models/master_models/prediction.py
+++ models/master_models/prediction.py
@@ -52,3 +52,20 @@ def position_dense():
     model.add(Activation('linear'))
 
     return model
+
+def project_model():
+    """ The same model as used for classification, but without
+    the top dense layers.
+    """
+
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3,3), activation = 'relu', input_shape= (16,16,1)))
+    model.add(Conv2D(64, (3,3), activation = 'relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.25))
+    model.add(Dense(128, activation = 'relu'))
+    model.add(Dropout(0.5))
+    model.add(Flatten())
+    model.add(Dense(4, activation = 'linear'))
+
+    return model
