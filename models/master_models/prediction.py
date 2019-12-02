aeef3760b58dddbeb2b8af4af5a52be375b7ec91
Geir Ulvik <ulvik@usit.uio.no>
2019-12-02 12:49:34 +0100

    Was idiot, learning rate of 1e3 is not recommended.

--- models/master_models/prediction.py
+++ models/master_models/prediction.py
@@ -21,7 +21,7 @@ def position_cnn(input_shape=(16,16,1)):
     #model.add(Dropout(0.25))
     
     model.add(Flatten())
-    model.add(Dense(1600, activation='relu'))
+    model.add(Dense(512, activation='relu'))
     #model.add(Dropout(0.5))
     model.add(Dense(512, activation='relu'))
     model.add(Dense(4))
