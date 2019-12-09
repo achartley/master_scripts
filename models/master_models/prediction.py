37a803c7fecdb3026e899d892317616e9b02e1cd
Geir Ulvik <ulvik@usit.uio.no>
2019-12-09 18:15:16 +0100

    Reduced position_cnn to very basic.

--- models/master_models/prediction.py
+++ models/master_models/prediction.py
@@ -11,18 +11,8 @@ def position_cnn(input_shape=(16,16,1)):
     # Add layers
     model.add(Conv2D(32, (3, 3), padding='same',
                      input_shape=input_shape, activation='relu'))
-    model.add(Conv2D(32, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    #model.add(Dropout(0.25))
-
-    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    #model.add(MaxPooling2D(pool_size=(2, 2)))
-    #model.add(Dropout(0.25))
     
     model.add(Flatten())
-    model.add(Dense(512, activation='relu'))
-    #model.add(Dropout(0.5))
     model.add(Dense(512, activation='relu'))
     model.add(Dense(4))
     model.add(Activation('linear'))
@@ -94,3 +84,8 @@ def position_project():
     model.add(Dense(4, activation = 'linear'))
 
     return model
+
+
+if __name__ == "__main__":
+    model = position_cnn()
+    print(model.summary())
