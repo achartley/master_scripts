d6c4f43caad7d62539b7fccaf55d7f94bccf9525
Geir Tore Ulvik <ulvik@vepsebol.uio.no>
2020-05-09 19:02:51 +0200

    Callback works! Printout isn't all that pretty, but works OK with model.fit verbose=2

--- analysis_functions/analysis_functions/callbacks.py
+++ analysis_functions/analysis_functions/callbacks.py
@@ -1,19 +1,29 @@
 import tensorflow as tf
+import numpy as np
 from tensorflow.keras import backend
 
 
 class R2ScoreCallback(tf.keras.callbacks.Callback):
     def __init__(self, validation_data):
-        self.validation_data = validation_data
+        super(R2ScoreCallback, self).__init__()
+        self.val_x = validation_data[0]
+        self.val_y = validation_data[1]
 
-    def r2_score(y_true, y_pred):
+    """
+    def r2_score(self, y_true, y_pred):
         SS_res =  backend.sum(backend.square(y_true - y_pred))
         SS_tot = backend.sum(backend.square(y_true - backend.mean(y_true)))
         return ( 1 - SS_res/(SS_tot + backend.epsilon()) )
-
+    """
+    def r2_score(self, y_true, y_pred):
+        epsilon = np.finfo(np.float32).eps
+        SS_res =  np.sum(np.square(y_true - y_pred))
+        SS_tot = np.sum(np.square(y_true - np.mean(y_true)))
+        return ( 1 - SS_res/(SS_tot + epsilon) )
 
     def on_epoch_end(self, epoch, logs=None):
-        y_pred = model.predict(model.x)
-        r2 = r2_score(self.validation_data, y_pred)
-        print("R2 score: ", r2)
+        y_pred = self.model.predict(self.val_x)
+        r2 = self.r2_score(self.val_y, y_pred)
+        print("val_r2: {:.4f}".format(r2))
+
 
