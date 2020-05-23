3c835906826927bbc46d26011b6d7c742aaebb89
Geir Tore Ulvik <ulvik@vepsebol.uio.no>
2020-05-23 12:54:53 +0200

    Restructured master_scripts to one single module

--- models/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='master_models',
-    version='0.1.0',
-    author='Geir Tore Ulvik',
-    author_email='g.t.ulvik@fys.uio.no',
-    description='Scripts and functions for keras models used in these project working on scintillator data.',
-)
