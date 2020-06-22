c5112a75b1439f7e631682233caab21e4dca5736
Geir Tore Ulvik <ulvik@vepsebol.uio.no>
2020-06-22 14:31:02 +0200

    Adding setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='master_scripts',
+    version='0.1.0',
+    author='Geir Tore Ulvik',
+    author_email='g.t.ulvik@fys.uio.no',
+    description='Scripts and functions used in master thesis.',
+    install_requires=[
+        "Numpy >= 1.17",
+    ],
+)
