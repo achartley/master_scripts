Corrected normalization function

master_scripts/data_functions.py
@@ -344,9 +344,9 @@ def normalize_image_data(images):
     """ Takes an imported set of images and normalizes values to between
     0 and 1 using min-max scaling across the whole image set.
     """
-    img_term = np.amax(images) - np.amin(images)
-    img_mean = np.mean(images)
-    images = (images - img_mean) / img_term
+    img_max = np.amax(images)
+    img_min = np.amin(images)
+    images = (images - img_min) / (img_max - img_min)
     return images
 
 