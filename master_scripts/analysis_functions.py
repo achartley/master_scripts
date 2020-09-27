ae994db43cac086f9924c4450af2d6222586022d
Geir Tore Ulvik <ulvik@luring.uio.no>
2020-09-27 13:08:15 +0200

    Updated DSNT layer and added custom loss function

--- master_scripts/analysis_functions.py
+++ master_scripts/analysis_functions.py
@@ -5,6 +5,9 @@ from master_scripts.data_functions import (get_git_root, relative_energy,
                                            separation_distance,
                                            energy_difference,
                                            event_indices)
+from tensorflow.python.framework import ops
+from tensorflow.python.keras import backend as K
+from tensorflow.python.ops import math_ops
 
 
 def calc_kfold_accuracies(acc_list):
@@ -221,3 +224,26 @@ def anodedata_classification_table(experiment_id, data_name,
 
     if return_events:
         return events
+
+
+def dsnt_mse(y_true, y_pred):
+    """Computes the mean squared error between labels and predictions.
+    After computing the squared distance between the inputs, the mean value
+    over the last dimension is returned.
+    `loss = mean(square(y_true - y_pred), axis=-1)`
+    Standalone usage:
+    >>> y_true = np.random.randint(0, 2, size=(2, 3))
+    >>> y_pred = np.random.random(size=(2, 3))
+    >>> loss = tf.keras.losses.mean_squared_error(y_true, y_pred)
+    >>> assert loss.shape == (2,)
+    >>> assert np.array_equal(
+    ...     loss.numpy(), np.mean(np.square(y_true - y_pred), axis=-1))
+    Args:
+      y_true: Ground truth values. shape = `[batch_size, d0, .. dN]`.
+      y_pred: The predicted values. shape = `[batch_size, d0, .. dN]`.
+    Returns:
+      Mean squared error values. shape = `[batch_size, d0, .. dN-1]`.
+    """
+    y_pred = ops.convert_to_tensor_v2(y_pred[1])
+    y_true = math_ops.cast(y_true, y_pred.dtype)
+    return K.mean(math_ops.squared_difference(y_pred, y_true), axis=-1)
