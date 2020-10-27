0c2eca22bdd27c34cb2899a5aa849108ab7fa58d
Geir Ulvik <g.t.ulvik@fys.uio.no>
2020-10-27 10:41:00 +0100

    Small update to pretrained models

--- master_scripts/models_pretrained.py
+++ master_scripts/models_pretrained.py
@@ -1,42 +1,40 @@
-import numpy as np
 from importlib import import_module
-import tensorflow as tf
-import tensorflow.keras.applications as tfapps
 from tensorflow.keras import Input
-from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Flatten
 
-def pretrained_model(which_model="VGG16", input_dim=(16, 16, 3), output_depth=None):
-    """ Setup an instance of any model available in tensorflow.keras with 
-        new input dimensions and a custom depth at which to get the output. 
-        The goal of this model is to extract features from the new input using 
-        pretrained weights from imagenet. 
+
+def pretrained_model(which_model="VGG16", input_dim=(16, 16, 3),
+                     output_depth=None):
+    """ Setup an instance of any model available in tensorflow.keras with
+        new input dimensions and a custom depth at which to get the output.
+        The goal of this model is to extract features from the new input using
+        pretrained weights from imagenet.
 
         param which_model:  string, which model to use. Defaults to VGG16
-        param input_dim:    dimensions of input, (16,16,3) is default for scintillator
-        param output_depth: At which layer to get the output from. If not provided
-                            returns full model (default) up to, but not including the
-                            dense layers. ("include_top=False")
-                            Same happens if provided depth is larger than number
-                            of layers.
+        param input_dim:    dimensions of input, default to (16, 16, 3)
+        param output_depth: At which layer to get the output from. If not
+            provided returns full model (default) up to, but not including the
+            dense layers ("include_top=False") Same happens if provided depth
+            is larger than number of layers.
     """
 
     # Dictionary of possible models and their import statements
     available_models = {
-            "DenseNet121":".densenet",
-            "DenseNet169":".densenet",
-            "DenseNet201":".densenet",
-            "InceptionResNetV2":".inception_resnet_v2",
-            "InceptionV3":".inception_v3",
-            "MobileNet":".mobilenet",
-            "MobileNetV2":".mobilenet_v2",
-            "NASNetLarge":".nasnet",
-            "NASNetMobile":".nasnet",
-            "ResNet50":".resnet50",
-            "VGG16":".vgg16",
-            "VGG19":".vgg19",
-            "Xception":".xception",
-            }
+        "DenseNet121": ".densenet",
+        "DenseNet169": ".densenet",
+        "DenseNet201": ".densenet",
+        "InceptionResNetV2": ".inception_resnet_v2",
+        "InceptionV3": ".inception_v3",
+        "MobileNet": ".mobilenet",
+        "MobileNetV2": ".mobilenet_v2",
+        "NASNetLarge": ".nasnet",
+        "NASNetMobile": ".nasnet",
+        "ResNet50": ".resnet50",
+        "VGG16": ".vgg16",
+        "VGG19": ".vgg19",
+        "Xception": ".xception",
+    }
 
     # Check if input model is valid
     if which_model not in available_models.keys():
@@ -50,7 +48,8 @@ def pretrained_model(which_model="VGG16", input_dim=(16, 16, 3), output_depth=No
     module = import_module(module_name)
 
     # Load the actual function which lets us create a new instance of a model
-    pretrained = getattr(module, which_model)(include_top=False, weights='imagenet')
+    pretrained = getattr(module, which_model)(
+        include_top=False, weights='imagenet')
 
     # Create new input layer
     input_layer = Input(shape=input_dim)
@@ -85,10 +84,13 @@ def pretrained_model(which_model="VGG16", input_dim=(16, 16, 3), output_depth=No
 
     return model
 
-def pretrained_vgg16(input_dim=(16, 16, 3)):
-    """ Setup an instance of vgg16 pretrained on imagenet
 
-        param input_dim:    dimensions of input, (16,16,3) is default for scintillator
+def pretrained_vgg16(input_dim=(16, 16, 3)):
+    """ Setup an instance of vgg16 pretrained on imagenet.
+
+        :param input_dim:    dimensions of input, (16,16,3) is default for
+                            scintillator
+        :returns model: Non-compiled Sequential model with pretrained layers.
     """
 
     # import correct module
@@ -103,8 +105,8 @@ def pretrained_vgg16(input_dim=(16, 16, 3)):
 
     # Add input layer and all pretrained layers except final softmax layer
     model = Sequential()
-    #model.add(input_layer)
-    for i in range(0, len(pretrained.layers)-1):
+    model.add(input_layer)
+    for i in range(0, len(pretrained.layers) - 1):
         try:
             model.add(pretrained.layers[i])
         except IndexError:
@@ -117,27 +119,29 @@ def pretrained_vgg16(input_dim=(16, 16, 3)):
 
     return model
 
+
 def pretrained_resnet50(input_dim=(16, 16, 3)):
     """ Setup an instance of resnet50 pretrained on imagenet
 
-        param input_dim:    dimensions of input, (16,16,3) is default for scintillator
+        param input_dim:    dimensions of input, (16,16,3) is default for
+                            scintillator
     """
 
     # import correct module
     module_name = "tensorflow.keras.applications.resnet50"
     module = import_module(module_name)
 
-
     # Load the actual function which lets us create a new instance of a model
-    pretrained = getattr(module, "ResNet50")(include_top=True, weights='imagenet')
+    pretrained = getattr(module, "ResNet50")(
+        include_top=True, weights='imagenet')
 
     # Create new input layer
     input_layer = Input(shape=input_dim)
 
     # Add input layer and all pretrained layers except final softmax layer
     model = Sequential()
-    #model.add(input_layer)
-    for i in range(0, len(pretrained.layers)-1):
+    model.add(input_layer)
+    for i in range(0, len(pretrained.layers) - 1):
         try:
             model.add(pretrained.layers[i])
         except IndexError:
